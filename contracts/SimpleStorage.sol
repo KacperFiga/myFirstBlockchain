931c9b6912927c3243e33077af6975a96fd56f79
KacperFiga <kacper.figa.com@o2.pl>
2022-09-17 22:36:48 +0200

    add setFavoriteNumber function

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,6 +1,10 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.6;
 
 contract SimpleStorage{
-    
+    uint256 public favoriteNumber;
+
+    function setFavoriteNumber(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
 }
