66516fde16854b1b5098fa858ec749c1f8b4efb3
KacperFiga <kacper.figa.com@o2.pl>
2022-09-19 14:31:32 +0200

    add my first array

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.6;
 contract SimpleStorage {
     uint256 favoriteNumber;
 
-    struct People{
+    struct People {
         uint256 favoriteNumber;
         string name;
     }
-    
-    People public person = People({favoriteNumber:47, name:"Kacper"});
+
+    People public person = People({favoriteNumber: 47, name: "Kacper"});
+
+    People[] public people;
 
     function setFavoriteNumber(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
@@ -17,5 +19,14 @@ contract SimpleStorage {
 
     function retrive() public view returns (uint256) {
         return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        People memory newPerson = People({
+            favoriteNumber: _favoriteNumber,
+            name: _name
+        });
+
+        people.push(newPerson);
     }
 }
