537bc404dc57952b178e9c33c2e305bd60a4dc0b
KacperFiga <kacper.figa.com@o2.pl>
2022-09-19 14:10:50 +0200

    myFirstStruct

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,10 +1,21 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.6;
 
-contract SimpleStorage{
-    uint256 public favoriteNumber;
+contract SimpleStorage {
+    uint256 favoriteNumber;
+
+    struct People{
+        uint256 favoriteNumber;
+        string name;
+    }
+    
+    People public person = People({favoriteNumber:47, name:"Kacper"});
 
     function setFavoriteNumber(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
+    }
+
+    function retrive() public view returns (uint256) {
+        return favoriteNumber;
     }
 }
